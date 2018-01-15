4d359aadc0cb1e54702e79202f73ab1c321482fb
user <user@RANJANAVR>
2018-01-15 13:57:53 +0800

    controllers created

---
+++ Inventory/Inventory-mvc/Controllers/AdjustmentVoucherRecordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_mvc.Controllers
+{
+    public class AdjustmentVoucherRecordController : Controller
+    {
+        // GET: AdjustmentVoucherRecord
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventory/Inventory-mvc/Controllers/VoucherDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_mvc.Controllers
+{
+    public class VoucherDetailsController : Controller
+    {
+        // GET: VoucherDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
