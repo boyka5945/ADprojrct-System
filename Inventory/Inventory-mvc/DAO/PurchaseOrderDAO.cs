514dc6a8bb53b741bd5ffdc9bbb27cb97a518d02
czolbe88 <czolbe88@gmail.com>
2018-01-15 19:14:01 +0800

    Created PurchaseOrderDAO

--- Inventory/Inventory-mvc/DAO/PurchaseOrderDAO.cs
+++ Inventory/Inventory-mvc/DAO/PurchaseOrderDAO.cs
@@ -2,11 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Inventory_mvc.Models;
 
 namespace Inventory_mvc.DAO
 {
-    public class PurchaseOrderDAO
+    public class PurchaseOrderDAO : IPurchaseOrderDAO
     {
+        public bool AddNewPurchaseOrder(Purchase_Order_Record purchase_order_record)
+        {
+            using(StationeryModel Entity = new StationeryModel())
+            {
+                Entity.Purchase_Order_Record.Add(purchase_order_record);
+                Entity.SaveChanges();
+                return true;
 
+            }
+
+            
+        }
+
+        public bool DeletePurchaseOrder(int orderID)
+        {
+            using (StationeryModel Entity = new StationeryModel()) {
+
+                Purchase_Order_Record por = Entity.Purchase_Order_Record.Where(x => x.orderNo == orderID).First();
+                Entity.Purchase_Order_Record.Remove(por);
+                Entity.SaveChanges();
+                return true;
+                    }
+
+        }
+
+        public Purchase_Order_Record FindByOrderID(int orderID)
+        {
+            using (StationeryModel Entity = new StationeryModel())
+            {
+                Purchase_Order_Record por = Entity.Purchase_Order_Record.Where(x => x.orderNo == orderID).First();
+                return por;
+
+
+            }
+
+        }
+
+        public List<Purchase_Order_Record> GetAllPurchaseOrder()
+        {
+            using (StationeryModel Entity = new StationeryModel())
+            {
+                List<Purchase_Order_Record> por = Entity.Purchase_Order_Record.ToList();
+                return por;
+
+            }
+
+        }
+
+        public int UpdatePurchaseOrderInfo(Purchase_Order_Record purchase_order_record)
+        {
+
+           
+        }
     }
 }
