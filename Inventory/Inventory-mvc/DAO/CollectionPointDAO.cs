6a47f104ccdc24e93632b995b316eb1b2c049311
czolbe88 <czolbe88@gmail.com>
2018-01-15 17:42:46 +0800

    Added CollectionPointDAO

--- Inventory/Inventory-mvc/DAO/CollectionPointDAO.cs
+++ Inventory/Inventory-mvc/DAO/CollectionPointDAO.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Inventory_mvc.Models;
 
 namespace Inventory_mvc.DAO
 {
-    public class CollectionPoint
+    public class CollectionPoint : ICollectionPoint
     {
+        public Collection_Point FindByCollectionPointID(int id)
+        {
+            using (StationeryModel entity = new StationeryModel())
+            {
+                Collection_Point cp = entity.Collection_Point.Where(x => x.collectionPointID == id).First();
+                return cp;
+                
 
+            }
+
+
+        }
+
+        public List<Collection_Point> GetAllCollectionPoints()
+        {
+            using (StationeryModel entity = new StationeryModel())
+            {
+                List<Collection_Point> listAll = entity.Collection_Point.ToList<Collection_Point>();
+                return listAll;
+            }
+        }
     }
 }
