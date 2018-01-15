74c1cfcb9ea2043a23db24a0f6927708c139d807
CK Tan <tanck105@hotmail.com>
2018-01-15 13:08:43 +0800

    added unit test

---
+++ Inventory/InventoryUnitTestProject/DAOTest/DepartmentDAOTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Inventory_mvc.DAO;
+using Inventory_mvc.Models;
+
+namespace InventoryUnitTestProject
+{
+    [TestClass]
+    public class DepartmentDAOTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        /*
+        [TestMethod]
+        public void Create_Post_ReturnsViewIfModelStateIsNotValid()
+        {
+            // Arrange
+            HomeController controller = GetHomeController(new InMemoryContactRepository());
+            // Simply executing a method during a unit test does just that - executes a method, and no more. 
+            // The MVC pipeline doesn't run, so binding and validation don't run.
+            controller.ModelState.AddModelError("", "mock error message");
+            Contact model = GetContactNamed(1, "", "");
+
+            // Act
+            var result = (ViewResult)controller.Create(model);
+
+            // Assert
+            Assert.AreEqual("Create", result.ViewName);
+        }
+        */
+    }
+}
