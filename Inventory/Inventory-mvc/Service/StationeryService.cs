af94b8c6f8a2af46f8dd4fb070a60f962604da2c
user <user@RANJANAVR>
2018-01-16 11:21:18 +0800

    create

--- Inventory/Inventory-mvc/Service/StationeryService.cs
+++ Inventory/Inventory-mvc/Service/StationeryService.cs
@@ -1,4 +1,6 @@
 ﻿using Inventory_mvc.DAO;
+using Inventory_mvc.Models;
+using Inventory_mvc.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,21 @@ namespace Inventory_mvc.Service
         bool IStationeryService.AddNewStationery(StationeryViewModel stationeryVM)
         {
             return stationeryDAO.AddNewStationery(ConvertFromViewModel(stationeryVM));
+        }
+
+        private StationeryDAO ConvertFromViewModel(StationeryViewModel stationeryVM)
+        {
+            Stationery stationery = new Stationery();
+
+            stationery.itemCode = stationeryVM.ItemCode;
+            stationery.categoryID = stationeryVM.GSTNo;
+            supplier.supplierName = supplierVM.SupplierName;
+            supplier.contactName = supplierVM.ContactName;
+            supplier.phoneNo = (int)supplierVM.PhoneNo;
+            supplier.faxNo = supplierVM.FaxNo;
+            supplier.address = supplierVM.Address;
+
+            return stationery;
         }
     }
 }
