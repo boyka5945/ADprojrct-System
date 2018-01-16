39359336d3a492585e27bc7a81c04a9ef46e9b48
WesLeeWH <33190336+WesLeeWH@users.noreply.github.com>
2018-01-16 10:53:35 +0800

    for collection point

--- Inventory/Inventory-mvc/Service/CollectionPointService.cs
+++ Inventory/Inventory-mvc/Service/CollectionPointService.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Inventory_mvc.DAO;
+using Inventory_mvc.Models;
 
 namespace Inventory_mvc.Service
 {
     public class CollectionPointService : ICollectionPointService
     {
+        public List<Collection_Point> GetAllCollectionPoint()
+        {
+            CollectionPointDAO cDAO = new CollectionPointDAO();
+            return cDAO.GetAllCollectionPoints();
+        }
 
+        public Collection_Point GetCollectionPointByID(int collectionPointID)
+        {
+            CollectionPointDAO cDAO = new CollectionPointDAO();
+            return cDAO.FindByCollectionPointID(collectionPointID);
+        }
     }
 }
