ec771517b3af906d9a3118b0b6de87c9b2aaa8c4
HP <HP@HP>
2018-01-16 09:17:22 +0800

    Updated DepartmentController, DepartmentDAO, DepartmentService, IDepartmentService, ListDepartment

--- Inventory/Inventory-mvc/Controllers/DepartmentController.cs
+++ Inventory/Inventory-mvc/Controllers/DepartmentController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Inventory_mvc.Service;
+using Inventory_mvc.Models;
 
 namespace Inventory_mvc.Controllers
 {
@@ -12,6 +14,13 @@ namespace Inventory_mvc.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ListDepartment()
+        {
+            DepartmentService ds = new DepartmentService();
+            List<Department> model = ds.GetAllDepartment();
+            return View(model);
         }
     }
 }
