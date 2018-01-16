156a4e5842a64ab9ae9242c718c93652e6f4815e
Thiri <Thiri@DESKTOP-6MSFV2K>
2018-01-16 10:58:06 +0800

    updated User

---
+++ Inventory/Inventory-mvc/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory_mvc.Service;
+
+namespace Inventory_mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserService userService = new UserService();
+        // GET: User
+        public ActionResult UserList()
+        {
+            return View(userService.GetAllUser());
+        }
+    }
+}
