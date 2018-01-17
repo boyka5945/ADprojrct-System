54bf148af2a9f875a46b59165daae150eb678841
Boyka <boyka59452@gmail.com>
2018-01-17 13:27:40 +0800

    fix manageRequisiton controller

--- Inventory/Inventory-mvc/DAO/RequisitionRecordDAO.cs
+++ Inventory/Inventory-mvc/DAO/RequisitionRecordDAO.cs
@@ -45,15 +45,94 @@ namespace Inventory_mvc.DAO
             return true;
         }
 
-        public int UpdateRequisition(Requisition_Record purchase_order_record)
+        public int UpdateRequisition(Requisition_Record requisition_Record, string status)
+        {
+            Requisition_Record rr = new Requisition_Record();
+            StationeryModel entity = new StationeryModel();
+            rr = entity.Requisition_Record.Where(x => x.requisitionNo == requisition_Record.requisitionNo).First();
+            rr.status = status;
+            entity.SaveChanges();
+            return 0;
+        }
+
+        public int UpdateRequisitionDetails(string itemcode, int requisitionNo, int allocateQty)
         {
             StationeryModel entity = new StationeryModel();
-            return 10;
+            Requisition_Details rd = new Requisition_Details();
+            rd = entity.Requisition_Details.Where(x => x.itemCode == itemcode && x.requisitionNo == requisitionNo).First();
+            rd.allocatedQty = allocateQty;
+            entity.SaveChanges();
+            return 0;
         }
 
         public Boolean DeleteRequisition(int requisitionNo)
         {
             return true;
+        }
+
+        public List<Requisition_Details> GetDetailsByNO(int No=0)
+        {
+            StationeryModel entity = new StationeryModel();
+            if (No == 0)
+            {
+                return entity.Requisition_Details.ToList();
+            }
+            else
+            {
+                
+                return entity.Requisition_Record.Where(x => x.requisitionNo == No).First().Requisition_Details.ToList();
+            }
+        }
+
+        public  List<string> GetDetailsByGroup()
+        {
+            StationeryModel entity = new StationeryModel();
+            List<Requisition_Details> list = new List<Requisition_Details>();
+            List<string> list2 = new List<string>();
+          
+            var groups = entity.Requisition_Details.Select(x => new
+            {
+                itemcode = x.itemCode
+            }).ToList();
+            var newgroups = groups.Distinct().ToList();
+            foreach (var group in newgroups)
+            {
+                list2.Add(group.itemcode);
+            }
+            return list2;
+        }
+
+        public List<Requisition_Record> GetRecordByItemCode(string itemCode)
+        {
+            StationeryModel entity = new StationeryModel();
+            List<Requisition_Record> returnList = new List<Requisition_Record>();
+            List<Requisition_Details> list = entity.Requisition_Details.Where(x => x.itemCode == itemCode).ToList();
+            foreach(var l in list)
+            {
+                returnList.Add(FindByRequisitionNo(l.requisitionNo));
+            }
+            return returnList;
+        }
+
+        public int FindUnfulfilledQtyBy2Key(string itemcode, int requisionNo)
+        {
+            StationeryModel entity = new StationeryModel();
+            Requisition_Details rd = entity.Requisition_Details.Where(x => x.itemCode == itemcode && x.requisitionNo == requisionNo).First();
+            if (rd.fulfilledQty != null)
+            {
+                return (int)rd.qty - (int)rd.fulfilledQty;
+            }
+            else
+            {
+                return rd.qty;
+            }
+        }
+
+        public Requisition_Details FindDetailsBy2Key(string itemcode, int requisitionNo)
+        {
+            StationeryModel entity = new StationeryModel();
+            return entity.Requisition_Details.Where(x=>x.itemCode == itemcode && x.requisitionNo == requisitionNo).First();
+            
         }
     }
 }
---
+++ Inventory/Inventory-mvc/Models/BigModelView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_mvc.Models
+{
+    public class BigModelView
+    {
+ 
+        public string itemCode { get; set; }
+        public string description { get; set; }
+        public string retrievedQuantity { get; set; }
+        public int unfulfilledQty { get; set; }
+        public int allocateQty { get; set; }
+        public Requisition_Record requisitionRecord{ get; set; }
+
+    }
+}
