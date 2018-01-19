f321f33f7d9bff4703537c783ffb0777c7730ac8
Thiri <Thiri@DESKTOP-6MSFV2K>
2018-01-19 14:34:22 +0800

    commit

--- Inventory/Inventory-mvc/Controllers/UserController.cs
+++ Inventory/Inventory-mvc/Controllers/UserController.cs
@@ -16,11 +16,11 @@ namespace Inventory_mvc.Controllers
         public ActionResult UserList()
         {
             string name = HttpContext.User.Identity.Name;
-            if(name=="")
-            {
-                return RedirectToAction("Login", "Home");
-            }
-            User user = userService.FindByUserID(name);
+            //if(name=="")
+            //{
+            //    return RedirectToAction("Login", "Home");
+            //}
+            User user = userService.FindByUserID("S1000");
             List<User> model = userService.GetUserByDept(user);
           //  ViewBag.Roles = userService.FindAllRole(user.UserID);
             //ViewBag.alrDelegated = userService.AlrDelegated(id);
