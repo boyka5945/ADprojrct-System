9c9ecf65cef982c5518160a28037a45a15b22c0c
HP <HP@HP>
2018-01-20 15:22:51 +0800

    pagination for ListPurchaseOrders

--- Inventory/Inventory-mvc/Controllers/PurchaseController.cs
+++ Inventory/Inventory-mvc/Controllers/PurchaseController.cs
@@ -7,13 +7,14 @@ using Inventory_mvc.Service;
 using Inventory_mvc.Models;
 using Inventory_mvc.DAO;
 using Inventory_mvc.ViewModel;
+using PagedList;
 
 namespace Inventory_mvc.Controllers
 {
     public class PurchaseController : Controller
     {
         // GET: PurchaseDetails
-
+        
         IPurchaseOrderService pos = new PurchaseOrderService();
         Purchase_Order_Record por = new Purchase_Order_Record();
         IStationeryService ss = new StationeryService();
@@ -27,9 +28,10 @@ namespace Inventory_mvc.Controllers
 
         [HttpGet]
         //FormCollection form
-        public ActionResult ListPurchaseOrders(string search, string searchBy)
+        public ActionResult ListPurchaseOrders(string search, string searchBy,int? page)
         {
-
+            ViewBag.Search = search;
+            ViewBag.SearchBy = searchBy;
             List<Purchase_Order_Record> model = new List<Purchase_Order_Record>();
             List<Purchase_Order_Record> searchResults = new List<Purchase_Order_Record>();
             switch (searchBy)
@@ -54,7 +56,10 @@ namespace Inventory_mvc.Controllers
                     break;
 
             }
-            return View(model);
+            int pageSize = 2;
+            int pageNumber = (page ?? 1);
+            return View(model.ToPagedList(pageNumber, pageSize));
+            //return View(model);
         }
 
         [HttpGet]
@@ -94,6 +99,9 @@ namespace Inventory_mvc.Controllers
 
             int orderNo = findNextOrderNo();
             ViewBag.orderNo = orderNo;
+
+            ViewBag.itemCode = pos.GetAllPurchaseOrder();
+
             List<Purchase_Detail> model = new List<Purchase_Detail>();
             Dictionary<Purchase_Detail, string> details = new Dictionary<Purchase_Detail, string>();
 
@@ -105,6 +113,12 @@ namespace Inventory_mvc.Controllers
 
             }
 
+            if(pd.itemCode == null)
+            {
+                string errorMessage = String.Format("Item Code must not be empty.");
+                ModelState.AddModelError("itemCode", errorMessage);
+                return View();
+            }
 
             details.Add(pd, supplierCode);
             model.Add(pd);
