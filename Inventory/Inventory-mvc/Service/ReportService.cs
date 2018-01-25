9517c71c1bb71ef29b752e1e5532d3291ed36378
Thiri <Thiri@DESKTOP-6MSFV2K>
2018-01-25 12:22:49 +0800

    chart update

---
+++ Inventory/Inventory-mvc/Service/ReportService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Inventory_mvc.DAO;
+using System.Web.Mvc;
+using Inventory_mvc.Models;
+using Inventory_mvc.ViewModel;
+
+namespace Inventory_mvc.Service
+{
+    public class ReportService : IReportService
+    {
+        IReportDAO reportDAO = new ReportDAO();
+        IStationeryService stationeryService = new StationeryService();
+        
+        public List<ReportViewModel> RetrieveQty()
+        {
+            List<Purchase_Detail> details = reportDAO.RetrieveQty();
+
+            List<ReportViewModel> vmList = new List<ReportViewModel>();
+            foreach (var d in details)
+            {
+                Stationery s = stationeryService.FindStationeryByItemCode(d.itemCode);
+
+                ReportViewModel vm = new ReportViewModel();
+                vm.Stationery = s;
+                vm.ItemCode = d.itemCode;
+                vm.Qty = d.qty;
+                vm.CategoryName = s.Category.categoryName;
+
+                vmList.Add(vm);
+            }
+            return vmList;
+        }
+    }
+}
