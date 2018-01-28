89486953c0f13e1418020d0aecac4272d1a9b308
CK Tan <tanck105@hotmail.com>
2018-01-28 23:18:22 +0800

    reports

--- Inventory/Inventory-mvc/Service/IReportService.cs
+++ Inventory/Inventory-mvc/Service/IReportService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Inventory_mvc.DAO;
+using Inventory_mvc.Models;
 using Inventory_mvc.Service;
 using Inventory_mvc.ViewModel;
 
@@ -22,12 +23,13 @@ namespace Inventory_mvc.Service
 
         List<ReportViewModel> GetApprovedRequisitionDetialsBasedOnYearAndMonth(int year, int month);
 
-
         // TODO - REMOVE THIS METHOD
         void GenerateRandomDataForRequisitionRecords();
+
         List<int> GetSelectableMonths(int year);
 
         int GetEarliestYear();
 
+        List<ReportViewModel> GetDepartmentApprovedRequisitionsOfYear(int year, string deptCode);
     }
 }
