3ce5902b6e7d1d0ec0d5d49d68a98e78e06f9af2
PaingSu <PaingSu@DESKTOP-O9D0JHF>
2018-02-02 16:44:20 +0800

    changePassword

--- Inventory/Inventory-mvc/Controllers/UserController.cs
+++ Inventory/Inventory-mvc/Controllers/UserController.cs
@@ -250,7 +250,7 @@ namespace Inventory_mvc.Controllers
             
             if (ModelState.IsValid)
             {
-                bool cond1 = userService.isSame(password, oldPassword);
+                bool cond1 = userService.isSame(Encrypt.DecryptMethod(password), oldPassword);
                 if (cond1)
                 {
                     //string errorMessage = String.Format("{0} Incorrect", oldPassword);
@@ -258,7 +258,7 @@ namespace Inventory_mvc.Controllers
 
                     TempData["IncorrectPassword"] = String.Format("Incorrect Password.");
                 }
-                bool cond2 = userService.isSame(password, newPassword);
+                bool cond2 = userService.isSame(Encrypt.DecryptMethod(password), newPassword);
                 if (!cond2)
                 {
                     TempData["SameWithOldPassword"] = String.Format("Same With Password.");
@@ -272,8 +272,9 @@ namespace Inventory_mvc.Controllers
                 {
                     try
                     {
+
                         bool t = userService.changePassword(changePasswordVM);
-                        if (userService.changePassword(changePasswordVM))
+                        if (!t)
                         {
                             TempData["ErrorMessage"] = String.Format("There is not change to '{0}'.", code);
                             
